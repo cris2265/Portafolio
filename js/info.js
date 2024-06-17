caf79030f97b72d3328c3a75c0321d5695a02193
cris2265 <cehernandez@scl.edu.gt>
2024-06-17 10:28:52 -0600

    Finish

--- js/info.js
+++ js/info.js
@@ -2,6 +2,7 @@ let perfil = `
     <div class="nombre_usuario">
         <div class="logo">CH</div>
         <span>Cehernandez</span>
+        <h4 class="informacion_2">Soy un estudiante aprendiendo a como utilizar diferentes lenguajes de programacion que sigue aprendiendo dia a dia ya que la tecnologia no descansa</h4>
     </div>
     <div class="conocimientos">
         <a href="" class="logos">
